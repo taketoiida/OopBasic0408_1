319711dc9d32f98530fa880a13c2b2510b1819f0
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-08 12:29:48 +0900

    OopBasic_1_0408_1229

--- OopBasic0408_1/src/company/ObjectSample.java
+++ OopBasic0408_1/src/company/ObjectSample.java
@@ -8,16 +8,20 @@ public class ObjectSample {
 
     public static void main(String[] args) {
         
-        var employee = new Employee("鈴木", "営業部", "課長", 100);
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木", department, "課長", 100);
         
         employee.report();
         employee.report(2);
+        employee.joinMeeting();
         
         System.out.println("");
         
-        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Employee("田中", devDepartment, "一般社員", 88);
         
-        engineer.report(3);
+        engineer.report();
+        engineer.joinMeeting();
                 // TODO 自動生成されたメソッド・スタブ
 
     }
